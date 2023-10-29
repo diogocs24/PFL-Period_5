cb50ca0b926fa5dbbeb537e661304417f67f7dc6
diogocs18 <diogopinguinhas@hotmail.com>
2023-10-29 19:21:44 +0000

    coisas

---
+++ auxiliar.pl
@@ -0,0 +1,11 @@
+:- use_module(library(between)).
+
+% input without using dot (professor's code)
+read_number(X):-
+    read_number_aux(X,0).
+read_number_aux(X,Acc):- 
+    get_code(C),
+    between(48, 57, C), !,
+    Acc1 is 10*Acc + (C - 48),
+    read_number_aux(X,Acc1).
+read_number_aux(X,X).
--- period5.pl
+++ period5.pl
@@ -1,10 +1,10 @@
 :- consult('menu.pl').
 :- consult('content.pl').
 :- consult('display_board.pl').
+:- consult('auxiliar.pl').
 :- use_module(library(lists)).
 :- use_module(library(random)).
 
 period5 :-     
-      firstMenu,
-      print_board.
+      firstMenu.
       
